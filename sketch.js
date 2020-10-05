c834c287f4c47c5f4bdb842fbb6da4377e6c8842
MuthuAmuthan <72019415+MuthuAmuthan@users.noreply.github.com>
2020-10-05 16:44:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,24 @@
 var fixedRect, movingRect;
 
-
 function setup() {
-  createCanvas(800,400);
-  fixedRect = createSprite(400, 200, 50, 80);
-  movingRect = createSprite(400, 200, 80, 50);
-  fixedRect.shapeColor = "red";
-  movingRect.shapeColor = "blue";
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(255,255,255); 
-  movingRect.x = mouseX;
-  movingRect.y = mouseY;
-  
-  if(fixedRect.x -movingRect.x < movingRect.width/2 + fixedRect.width/2
-    && movingRect.x - fixedRect.x  < movingRect.width/2 + fixedRect.width/2
-    && fixedRect.y -movingRect.y < movingRect.height/2 + fixedRect.height/2
-    && movingRect.y - fixedRect.y  < movingRect.height/2 + fixedRect.height/2){
-    fixedRect.shapeColor = "green";
-    movingRect.shapeColor = "green";
+  background(0,0,0);  
 
-  }
-  else{
-    fixedRect.shapeColor = "red";
-    movingRect.shapeColor = "blue";
-  }
+  bounceOff(movingRect,fixedRect);
+
+ 
   drawSprites();
 }
+
--- sketch1.js
+++ sketch1.js
@@ -3,34 +3,27 @@ var fixedRect, movingRect;
 
 function setup() {
   createCanvas(800,400);
-  fixedRect = createSprite(700, 200, 50, 80);
-  movingRect = createSprite(100, 200, 80, 50);
-
-  fixedRect.velocityX = -3;
-  movingRect.velocityX = 3;
-
-
-
+  fixedRect = createSprite(400, 200, 50, 80);
+  movingRect = createSprite(400, 200, 80, 50);
   fixedRect.shapeColor = "red";
   movingRect.shapeColor = "blue";
 }
 
 function draw() {
   background(255,255,255); 
- 
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
   
-  if(fixedRect.x -movingRect.x < movingRect.width/2 + fixedRect.width/2
-    && movingRect.x - fixedRect.x  < movingRect.width/2 + fixedRect.width/2){
-        fixedRect.velocityX = fixedRect.velocityX * -1;
-        movingRect.velocityX = movingRect.velocityX * -1;
+  if(isTouching(movingRect, fixedRect)){
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
 
-    }
-    if(fixedRect.y -movingRect.y < movingRect.height/2 + fixedRect.height/2
-    && movingRect.y - fixedRect.y  < movingRect.height/2 + fixedRect.height/2){
-        fixedRect.velocityY = fixedRect.velocityY * -1;
-        movingRect.velocityY = movingRect.velocityY * -1;
-    
+  }
+  else{
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "blue";
+}
 
-    }
+
   drawSprites();
 }
